39548eb0d5798bf1f45add2a0589a49620ee6f06
Vinicius Giacomelli <viniciuscamargogiacomelli55@gmail.com>
2025-02-16 22:58:52 -0300

    feat(frontend): adiciona componentes de modal - Adiciona componente de item do modal de perfil - Adiciona componente de modal de perfil

---
+++ src/componentes/ModalPerfil/ModalPerfil.tsx
@@ -0,0 +1,13 @@
+import './ModalPerfil.css';
+
+
+const Modal = ({children} : any) => {
+    return (
+        <div className="modal-perfil">
+            {children}
+        </div>
+    );
+}
+
+export default Modal
+
