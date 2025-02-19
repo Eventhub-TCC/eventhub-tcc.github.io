10b26ba9f43b563e5bc8621d2f509535b987cfea
João Pedro Martins Forte <joao.forte@fatec.sp.gov.br>
2025-02-18 22:03:34 -0300

    feat(frontend): adiciona componente barra lateral
    - Adiciona o componente BarraLateral
    - Ajusta o ItemBarraLateral

--- src/componentes/ItemBarraLateral/ItemBarraLateral.tsx
+++ src/componentes/ItemBarraLateral/ItemBarraLateral.tsx
@@ -1,23 +1,27 @@
 import { NavLink } from "react-router"
 import "./ItemBarraLateral.css"
 
-const ItemBarraLateral = ({texto, icone, caminho, corSelecionado = "var(--purple-700)", children}: any) => {
+const ItemBarraLateral = ({minimizado = false, texto, icone, caminho, corSelecionado = "var(--purple-700)", children}: any) => {
     return (
-        <NavLink 
-            to={caminho} 
-            className={({isActive}: any) => (`item-barra ${isActive ? "item-barra--ativo" : ''}`)} 
-            style={{'--cor-barra-lateral': corSelecionado} as React.CSSProperties}
-        >
-            <div className="item-barra__icone-container">
-                {
-                    icone ? <i className={`${icone} item-barra__icone`}></i> : ''
-                }
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                    {children ? children : ''}
-                </svg>
-            </div>
-            <span className="item-barra__texto">{texto}</span>
-        </NavLink>
+      <li className="item-barra">
+          <NavLink 
+              to={caminho} 
+              className={({isActive}: any) => (`item-barra__link ${isActive ? "item-barra__link--ativo" : ''}`)} 
+              style={{'--cor-barra-lateral': corSelecionado} as React.CSSProperties}
+          >
+              <div className="item-barra__icone-container">
+                  {
+                      icone ? <i className={`${icone} item-barra__icone`}></i> : ''
+                  }
+                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                      {children ? children : ''}
+                  </svg>
+              </div>
+              {
+                !minimizado ? <span className="item-barra__texto">{texto}</span> : ''
+              }
+          </NavLink>
+      </li>
     )
 }
 
