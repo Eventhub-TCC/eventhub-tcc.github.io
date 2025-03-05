21784b62b29cdbe2a5f199e6e281ed588c9d6dcf
João Pedro Martins Forte <joao.forte@fatec.sp.gov.br>
2025-03-05 16:01:54 -0300

    feat(senha): cria pagina esqueci minha senha
    - Rota /esqueci-senha criada
    - Página de esqueci minha senha criada
    - Instrução agora é um componente e foi ajustada no cadastro do usuário

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Login from './paginas/Login/Login'
 import CadastroUsuario from './paginas/CadastroUsuario/CadastroUsuario'
 import CadastroEvento from './paginas/CadastroEvento/CadastroEvento'
 import UsuarioLayout from './componentes/UsuarioLayout/UsuarioLayout'
+import EsqueciSenha from './paginas/EsqueciSenha/EsqueciSenha'
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
         <Route element={<UsuarioLayout />}>
           <Route path='login' element={<Login />}/>
           <Route path='cadastro' element={<CadastroUsuario />}/>
+          <Route path='esqueci-senha' element={<EsqueciSenha />}/>
         </Route>
         <Route path='criar-evento' element={<CadastroEvento />}/>
       </Routes>
