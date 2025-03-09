81d690d79d8dc9341a813711540d53ab643830c1
Vinicius Giacomelli <viniciuscamargogiacomelli55@gmail.com>
2025-03-09 00:34:27 -0300

    feat(frontend): cria organizador de layout - Cria componente padrao para layout quando usuario organizador logado - Ajusta barra lateral para se adequar ao novo layout

--- src/componentes/BarraLateral/BarraLateral.tsx
+++ src/componentes/BarraLateral/BarraLateral.tsx
@@ -1,7 +1,7 @@
 import { useState, Children, cloneElement } from 'react'
 import './BarraLateral.css'
 
-const BarraLateral = ({cor = 'var(--purple-800)', corHoverMinimizar = 'var(--purple-700)', children}: any) => {
+const BarraLateral = ({enviaMinimizada, cor = 'var(--purple-800)', corHoverMinimizar = 'var(--purple-700)', children}: any) => {
   const [ minimizado, setMinimizado ] = useState(false);
   
   return (
@@ -15,7 +15,9 @@ const BarraLateral = ({cor = 'var(--purple-800)', corHoverMinimizar = 'var(--pur
         <button 
           style={{'--cor-minimizar': corHoverMinimizar} as React.CSSProperties} 
           className='barra-lateral__botao-minimizar' 
-          onClick={() => setMinimizado(!minimizado)}
+          onClick={() => {setMinimizado(!minimizado)
+            enviaMinimizada(!minimizado)
+          }}
         >
           <svg className={minimizado ? `barra-lateral__icone-gira` : ''} xmlns="http://www.w3.org/2000/svg" width="26" height="24" viewBox="0 0 26 24" fill="none">
             <path d="M11.5 17.2L5.5 11.6L11.5 6M20.5 17.2L14.5 11.6L20.5 6"/>
